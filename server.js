9a36de7be5084012acf05d9c6cbeefc10086b848
Red3ffect <red3ffect117@gmail.com>
2021-05-29 17:58:47 -0700

    Adds front end files and requests where they should be in the js files

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const { allowedNodeEnvironmentFlags } = require('process');
+const { serialize } = require('v8');
 
 // parse incoming string or array  data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// makes static front-end resources readily available
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals.json');
 
@@ -111,6 +115,22 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
